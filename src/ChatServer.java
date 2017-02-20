358bd5b16836251ea3ad22f57461c7785899cc66
PhuongLab <phuong.quan.thai@gmail.com>
2017-02-20 14:03:43 +0100

    Merge branch 'master' of C:\Users\phuon\IdeaProjects\group_chat with conflicts.

--- src/ChatServer.java
+++ src/ChatServer.java
@@ -1,36 +1,5 @@
-/*
- * Phuong Quan Thai & Simon Konstantyner
- * Dat16v2
- *
- * Mandatory Assignment 1(SWC3 + TECH2)
- * - Chat system with multiple clients
- */
-
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.TextArea;
-import javafx.stage.Stage;
-
-public class ChatServer extends Application {
-    @Override
-    public void start( Stage primaryStage ) throws Exception {
-        //Set window
-        Stage window = primaryStage;
-        window.setTitle( "Chat Server" );
-
-        //Create Server Log area
-        TextArea serverLog = new TextArea();
-        serverLog.setEditable( false );
-
-        //Create Scene
-        Scene scene = new Scene( serverLog, 500, 700 );
-
-        //Set Scene and Show Stage
-        window.setScene( scene );
-        window.show();
-    }
-
+public class ChatServer {
     public static void main( String[] args ) {
-        launch( args );
+         System.out.println("hey");
     }
 }
